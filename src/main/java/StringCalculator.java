71ea0981ececea4d73103eea8478fe9235659ff1
KyungSik9870 <rudtlr9870@gmail.com>
2021-09-21 12:23:35 +0900

    feat: '콤마를 구분자로 각 숫자의 합을 계산' 테스트 통과하도록 작성
    TDD 의 사이클 중 두번째, 테스트 통과하도록 프로덕션 코드 작성

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,9 +1,16 @@
 public class StringCalculator {
 
 	public int splitAndSum(String input) {
+		int result = 0;
+
 		if (input == null || input.isEmpty()) {
 			return 0;
+		} else {
+			String[] inputs = input.split(",");
+			for (String number : inputs) {
+				result += Integer.parseInt(number);
+			}
 		}
-		return Integer.parseInt(input);
+		return result;
 	}
 }
